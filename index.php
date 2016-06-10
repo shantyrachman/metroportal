0310ca59b8cd5821420901c9324d2772591e0abb
shantyrachman <shanty.rachman@yahoo.com>
2016-06-10 11:27:53 +0800

    index
    perbaikan

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Bootbusiness | Short description about company">
     <meta name="author" content="Your name">
-    <title>Bootbusiness | Give unique title of the page here</title>
+    <title>Portal Berita | Berita terbaru</title>
     <!-- Bootstrap -->
     <link href="asset/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap responsive -->
